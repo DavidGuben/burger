60b11d4f2d43d2a40d6a0b6d993ed23ee27cbae3
DavidGuben <davidjg6@gmail.com>
2016-08-05 18:03:51 -0400

    added basic server code

--- server.js
+++ server.js
@@ -5,7 +5,19 @@ var methodOverride  = require('method-override');
 var app = express();
 
 
-app.get('/', function(){
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 
+var routes = require('./controllers/cats_controller.js');
+app.use('/', routes);
 
-});
+var port = 3000;
+app.listen(port);
