11ee03f576760657d25b3d147b0a48398944c8ae
David <david@Davids-MacBook-Pro-2.local>
2016-09-02 12:54:36 -0400

    server up and running listening on port 3000

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+var app = express();
+
+// serves static content to the app from the public directory
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(methodOverride('_method'));
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+// var routes = require('./controllers/burger_controller.js');
+app.use('/', function() {
+  console.log('works');
+});
+
+var port = 3000;
+app.listen(port, function(){
+  console.log('listening on port ' + port);
+});
