735e15fce7864442cd2513fcb53d8bd12dbce96e
David <david@Davids-MacBook-Pro-2.local>
2016-09-02 13:06:32 -0400

    added initial get and post routes to controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var cat = require('../models/burger.js');
+
+router.get('/', function(req, res) {
+  res.redirect('/burgers');
+});
+
+router.get('/burgers', function(req, res) {
+  burger.all(function (data) {
+    var hbsObect = { cats:data };
+    console.log(hbsObject);
+    res.render('index', hbsObject);
+  });
+});
+
+router.post('/burgers/create', function(req, res) {
+  cat.create(['name', 'devoured'], [req.body.name, req.body.devoured], function() {
+    res.redirect('/burgers');
+  });
+});
